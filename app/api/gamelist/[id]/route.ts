5d7440e824ac00f4c8b8501d94ce1bb05006f7d2
David Josipović <84419254+davidjosipovic@users.noreply.github.com>
2023-11-10 23:22:04 +0100

    Update route.ts

--- app/api/gamelist/[id]/route.ts
+++ app/api/gamelist/[id]/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
-
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
