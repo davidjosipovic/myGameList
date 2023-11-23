4df776459ab070e6691aa811933c8a9ff5f1fe44
David Josipović <davidjosipovic19@gmail.com>
2023-11-23 12:45:19 +0100

    buttons

--- components/VideoGallery.tsx
+++ components/VideoGallery.tsx
@@ -28,26 +28,27 @@ const VideoGallery = ({ game }) => {
                 title={`Video ${index}`}
                 className="w-full aspect-video h-full p-0.5"
               ></iframe>
-              <div className="absolute inset-0 flex items-center">
-                {index === 0 && (
-                  <button
-                    onClick={handlePrevPage}
-                    disabled={currentPage === 1}
-                    className="px-4 py-2 bg-blue-500 text-white"
-                  >
-                    Previous
-                  </button>
-                )}
-                {index === 1 && (
-                  <button
-                    onClick={handleNextPage}
-                    disabled={currentPage === Math.ceil(game.videos.length / videosPerPage)}
-                    className="ml-auto px-4 py-2 bg-blue-500 text-white"
-                  >
-                    Next
-                  </button>
-                )}
-              </div>
+              {game.videos.length > 2 && (
+                <div className="absolute inset-0 flex items-center">
+                  {index === 0 && currentPage > 1 && (
+                    <button
+                      onClick={handlePrevPage}
+                      disabled={currentPage === 1}
+                      className="px-4 py-2 "
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 256 512" className='fill-white'><path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"/></svg>  
+                    </button>
+                  )}
+                  {index === 1 && endIndex < game.videos.length && (
+                    <button
+                      onClick={handleNextPage}
+                      className="ml-auto px-4 py-2"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" height="3em" viewBox="0 0 256 512" className='fill-white'><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg>
+                    </button>
+                  )}
+                </div>
+              )}
             </div>
           ))}
       </div>
